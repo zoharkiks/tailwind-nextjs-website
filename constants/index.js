12fed89c0dcd983d1046bbfb844b488d0d4cdb86
Zohar Williams <61095857+zoharkiks@users.noreply.github.com>
2022-07-25 13:08:14 +0530

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,3 @@
 // Import all constants like images here for easy exports
-
-// export default {graph1,graph2, hero,img1,img2,img3,info1,info2,info3,logo}
+//  import ... from "..."
+// export default {}
